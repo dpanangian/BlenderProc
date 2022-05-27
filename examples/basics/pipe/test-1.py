db06b5d99f4ca6f803f7dfe6abcec92c6a5562ca
Daniel Panangian <dan.sibarani@gmail.com>
2022-05-27 18:29:04 +0200

    update

--- examples/basics/pipe/test-1.py
+++ examples/basics/pipe/test-1.py
@@ -1,13 +1,19 @@
 import blenderproc as bproc
+
+import debugpy
+debugpy.listen(5678)
+debugpy.wait_for_client()
+
+
+
 import numpy as np
 import bpy
 import os
 import random
 
-import pydevd_pycharm
-pydevd_pycharm.settrace('localhost', port=12345, stdoutToServer=True, stderrToServer=True)
 
 blend_path = "examples/basics/pipe/test-1.blend"
+segcolormap_path = "examples/basics/pipe/segmap.json"
 output_dir = "examples/basics/pipe/output"
 
 bproc.init()
@@ -28,12 +34,13 @@ light2.set_energy(1000)
 
 light3 = bproc.types.Light()
 light3.set_type("POINT")
-light3.set_location([2.58102, -2.18806, 5])
+light3.set_location([-0.342514 , -3.05966, 7.57489])
 light3.set_energy(10.6)
 light3.set_color([255, 0, 0])
 
 # define the camera resolution
-bproc.camera.set_resolution(1560, 1040)
+#bproc.camera.set_resolution(1560, 1040)
+bproc.camera.set_resolution(390, 260)
 bproc.camera.set_intrinsics_from_blender_params(lens=0.10)
 
 line = [0, -16.671, 4.0742, 1.57, 0, 0]
@@ -43,17 +50,18 @@ bproc.camera.add_camera_pose(matrix_world)
 
 # randomize screw distance and angle
 
-num_variations = 100
+num_variations = 2
 
 for variation in range(0, num_variations):
 
     bpy.context.view_layer.objects.active = bpy.data.objects['Wrap 2']
     bpy.context.object.modifiers["Screw"].screw_offset = random.uniform(5, 5.3)
+    bpy.context.object.modifiers["Screw"].angle = random.uniform(9.4, 9.5)
 
     data = bproc.renderer.render()
 
     # Render segmentation masks (per class and per instance)
-    data.update(bproc.renderer.render_segmap(map_by=["class", "instance", "name"]))
-
+    #data.update(bproc.renderer.render_segmap(map_by=["instance"], segcolormap_path=segcolormap_path, render_colorspace_size_per_dimension=255 ))
+    data.update(bproc.renderer.render_segmap(map_by=["cp_label","instance"]))
     # write the data to a .hdf5 container
     bproc.writer.write_hdf5(os.path.join(output_dir, str(variation)), data)
