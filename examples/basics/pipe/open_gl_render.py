f8aa8d5a4cf5648e0511ee1a4fe283783f9fecf6
Daniel Panangian <dan.sibarani@gmail.com>
2022-06-07 07:44:44 +0200

    update gl render

--- examples/basics/pipe/open_gl_render.py
+++ examples/basics/pipe/open_gl_render.py
@@ -1,5 +1,14 @@
 import bpy
 
 
-bpy.context.scene.render.filepath = "C:/Users/danie/Pictures/image.png"
-bpy.ops.render.render(write_still = True)
+my_areas = bpy.context.workspace.screens[0].areas
+my_shading = 'SOLID'  # 'WIREFRAME' 'SOLID' 'MATERIAL' 'RENDERED'
+
+for area in my_areas:
+    for space in area.spaces:
+        if space.type == 'VIEW_3D':
+            for shading in ['SOLID', 'MATERIAL']:
+                bpy.context.scene.render.filepath = "C:/Users/danie/Pictures/image_"+shading+".png"
+                space.shading.type = shading
+                area.spaces[0].region_3d.view_perspective = 'CAMERA'
+                bpy.ops.render.opengl(write_still=True)
